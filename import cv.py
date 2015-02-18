af20e36aeb88aee2dd0328976da0ee0d122c6e4f
Antoniojm <antonio93jm@gmail.com>
2015-02-18 20:32:45 +0100

    Rehacer código

--- import cv.py
+++ import cv.py
@@ -1,87 +1,171 @@
 import cv
 import numpy as np
 
-output = cv.LoadImage('4.jpg')
-orig = cv.LoadImage('4.jpg')
 
-#cambio
-#cambio2
+def channel_processing(channel):
+    pass
+    cv.AdaptiveThreshold(channel, channel, 255, adaptive_method=cv.CV_ADAPTIVE_THRESH_MEAN_C, thresholdType=cv.CV_THRESH_BINARY, blockSize=255, param1=25)
+    #mop up the dirt
+    cv.Dilate(channel, channel, None, 1)
+    cv.Erode(channel, channel, None, 1)
 
-#cambio4
+def draw_circles(storage, output):
+    circles = np.asarray(storage)
+    cuadrados = []
+    
+    for circle in circles:
+        Radius, x, y = int(circle[0][2]), int(circle[0][0]), int(circle[0][1])
+        x1 = x - Radius
+        x2 = x + Radius
+        y1 = y - Radius
+        y2 = y + Radius
+   
 
-#cambio3
+        vec = [x1, x2, y1, y2] 
 
+        cuadrado = output[y1:y2, x1:x2]        
+        cuadrados.append(cuadrado)
+       
+        # cv.Circle(output, (x, y), 1, cv.CV_RGB(0, 255, 0), -1, 8, 0)
+        # cv.Circle(output, (x, y), Radius, cv.CV_RGB(255, 0, 0), 3, 8, 0)
+        
+        # cv.ShowImage("pic{:>05}".format(circle),cuadrado)
+    
+    return cuadrados
+
+def moda(cuadrados):
+  
+    
+    aux = 0
+    cont = 0
+    moda = -1
+    cuadrados.sort(cmp=None, key=None, reverse=False)
+    print cuadrados
+    for i in range(0,len(cuadrados)-1):
+        if (cuadrados[i] == cuadrados[i+1]):
+            cont = cont + 1
+            if cont >= aux:
+                aux = cont
+                moda = cuadrados[i]
+        else:
+            cont=0
+ 
+    return moda
+    
+def modafinal (cuadrado):
+    r,g,b= dividir(cuadrado)
+    vr = vectorizar(r)
+    vv = vectorizar(g)
+    va = vectorizar(b)
+    m_r = moda(vr)
+    m_v = moda(vv)
+    m_a = moda(va)  
+
+    vaux = [m_r,m_v,m_a]
+    return vaux    
+
+def dividir (imagen):
+
+    rojo = cv.CreateImage((cuadrado1.width,cuadrado1.height), cv.IPL_DEPTH_8U, 1)
+    verde = cv.CreateImage((cuadrado1.width,cuadrado1.height), cv.IPL_DEPTH_8U, 1)
+    azul= cv.CreateImage((cuadrado1.width,cuadrado1.height), cv.IPL_DEPTH_8U, 1)
+    cv.Split(cuadrado1,rojo,verde,azul,None)
+    Ir = np.asarray(rojo[:,:])
+    Iv = np.asarray(verde[:,:])
+    Ia = np.asarray(azul[:,:])   
+    return [Ir,Iv,Ia]
+
+def vectorizar (vector):
+    v = []
+    for i in xrange(0,len(vector[0])-1):
+        for j in xrange(0,len(vector[0])-1):        
+            aux = vector[i][j]
+            v.append(aux)
+    return v
+
+
+output = cv.LoadImage('escaner-2.jpg')
+orig = cv.LoadImage('escaner-2.jpg')
 
 # create tmp images
 rrr=cv.CreateImage((orig.width,orig.height), cv.IPL_DEPTH_8U, 1)
 ggg=cv.CreateImage((orig.width,orig.height), cv.IPL_DEPTH_8U, 1)
 bbb=cv.CreateImage((orig.width,orig.height), cv.IPL_DEPTH_8U, 1)
 processed = cv.CreateImage((orig.width,orig.height), cv.IPL_DEPTH_8U, 1)
+
+
 storage = cv.CreateMat(orig.width, 1, cv.CV_32FC3)
 
-def channel_processing(channel):
-    pass
-    cv.AdaptiveThreshold(channel, channel, 255, adaptive_method=cv.CV_ADAPTIVE_THRESH_MEAN_C, thresholdType=cv.CV_THRESH_BINARY, blockSize=55, param1=7)
-    #mop up the dirt
-    cv.Dilate(channel, channel, None, 1)
-    cv.Erode(channel, channel, None, 1)
-
-def inter_centre_distance(x1,y1,x2,y2):
-    return ((x1-x2)**2 + (y1-y2)**2)**0.5
-
-def colliding_circles(circles):
-    for index1, circle1 in enumerate(circles):
-        for circle2 in circles[index1+1:]:
-            x1, y1, Radius1 = circle1[0]
-            x2, y2, Radius2 = circle2[0]
-            #collision or containment:
-            if inter_centre_distance(x1,y1,x2,y2) < Radius1 + Radius2:
-                return True
-
-def find_circles(processed, storage, LOW):
-    try:
-        cv.HoughCircles(processed, storage, cv.CV_HOUGH_GRADIENT, 2, 32.0, 30, LOW)#, 0, 100) great to add circle constraint sizes.
-    except:
-        LOW += 1
-        print 'try'
-        find_circles(processed, storage, LOW)
-    circles = np.asarray(storage)
-    print 'number of circles:', len(circles)
-    if colliding_circles(circles):
-        LOW += 1
-        storage = find_circles(processed, storage, LOW)
-    print 'c', LOW
-    return storage
-
-def draw_circles(storage, output):
-    circles = np.asarray(storage)
-    print len(circles), 'circles found'
-    for circle in circles:
-        Radius, x, y = int(circle[0][2]), int(circle[0][0]), int(circle[0][1])
-        cv.Circle(output, (x, y), 1, cv.CV_RGB(0, 255, 0), -1, 8, 0)
-        cv.Circle(output, (x, y), Radius, cv.CV_RGB(255, 0, 0), 3, 8, 0)
 
 #split image into RGB components
 cv.Split(orig,rrr,ggg,bbb,None)
-#process each component
+#process each componentsolar flare
 channel_processing(rrr)
 channel_processing(ggg)
 channel_processing(bbb)
 #combine images using logical 'And' to avoid saturation
 cv.And(rrr, ggg, rrr)
 cv.And(rrr, bbb, processed)
-cv.ShowImage('before canny', processed)
-# cv.SaveImage('case3_processed.jpg',processed)
+#cv.ShowImage('before canny', processed)
+
+
 #use canny, as HoughCircles seems to prefer ring like circles to filled ones.
 cv.Canny(processed, processed, 5, 70, 3)
 #smooth to reduce noise a bit more
 cv.Smooth(processed, processed, cv.CV_GAUSSIAN, 7, 7)
-cv.ShowImage('processed', processed)
-#find circles, with parameter search
-storage = find_circles(processed, storage, 100)
-draw_circles(storage, output)
-# show images
-cv.ShowImage("original with circles", output)
-cv.SaveImage('case1.jpg',output)
+#cv.ShowImage('processed', processed)
+
+#find circles 
+#escaner-1, escaner-2 ----> radiomax=115
+#escaner-3, escaner-4 ----> radiomax=135
+#3,4 ---------------------> radiomax=301
+#1,2 ---------------------> radiomax=150
+#sensor6.jpg--------------> radiomax=130
+
+radiomax=135
+cv.HoughCircles(processed, storage, cv.CV_HOUGH_GRADIENT, 2, 32, 100, radiomax)
+
+
+cuadrados = draw_circles(storage, output)
+
+
+# for x in xrange(0,len(cuadrados)):
+
+cuadrado1 = cuadrados[0]
+cuadrado2 = cuadrados[1]
+cuadrado3 = cuadrados[2]
+cuadrado4 = cuadrados[3]
+
+cv.ShowImage("Display window",cuadrado4)
+# cuadrado5 = cuadrados[4]
+# cuadrado6 = cuadrados[5]
+# cuadrado7 = cuadrados[6]
+# cuadrado8 = cuadrados[7]
+# cuadrado9 = cuadrados[8]
+# cuadrado10 = cuadrados[8]
+# cuadrado11= cuadrados[9]
+# cuadrado12 = cuadrados[10]
+# cuadrado13 = cuadrados[11] 
+# cuadrado14 = cuadrados[12]
+#a = modafinal(cuadrado3)
+#r1 =  np.array(r).tolist()
+
+
+
+    
+
+
+    
+    
+
+
+
+
+
+    
+     
+
+
+
 
 cv.WaitKey(0)
